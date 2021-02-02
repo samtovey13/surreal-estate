6e225a07221035e026e44c49e13f7204c0c4bce4
Sam Tovey <samtovey13@gmail.com>
2021-02-02 01:23:50 +0000

    created UI side of AddProperty form. Component currently prints form content to console on submit. Added new favicon to match icon.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div className="navbar">
-      <img classname="logo" src={logo} alt="logo" width="80" height="100" />
+      <img className="logo" src={logo} alt="logo" width="80" height="100" />
       <h1 className="header-name">Surreal Estate</h1>
       <ul className="navbar-links">
         <li className="navbar-links-item">
