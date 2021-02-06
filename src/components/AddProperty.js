3f56e631a52190a593eced7e79a770abb8950cd1
Sam Tovey <samtovey13@gmail.com>
2021-02-06 20:02:02 +0000

    restructured stateful values so that all state is now held at top level in App component

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,47 +1,45 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/AddProperty.css";
-import createProperty from '../requests/createProperty';
-import Alert from "./Alert";
+// import createProperty from '../requests/createProperty';
 
-const AddProperty = () => {
-  const initialState = {
-    fields: {
-      title: "",
-      type: "Flat",
-      bedrooms: "",
-      bathrooms: "",
-      price: "",
-      city: "Manchester",
-      email: "",
-    },
-    alert: {
-      message: "",
-      isSuccess: false,
-    },
-    hasBeenSubmitted: false
-  };
-  const [fields, setFields] = useState(initialState.fields);
-  const [alert, setAlert] = useState(initialState.alert);
+const AddProperty = ( fields, setFields, handleSubmit ) => {
+  // const initialState = {
+  //   fields: {
+  //     title: "",
+  //     type: "Flat",
+  //     bedrooms: "",
+  //     bathrooms: "",
+  //     price: "",
+  //     city: "Manchester",
+  //     email: "",
+  //   },
+  //   alert: {
+  //     message: "",
+  //     isSuccess: false,
+  //   },
+  // };
+  // const [fields, setFields] = useState(initialState.fields);
+  // const [alert, setAlert] = useState(initialState.alert);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setAlert({
-      message: "",
-      isSuccess: false,
-    });
-    const res = await createProperty(fields);
-    if (res.status === 201) {
-      setAlert({
-        message: "Success! Property added.",
-        isSuccess: true,
-      });
-    } else {
-      setAlert({
-        message: "Oops! Something went wrong. Property couldn't be added.",
-        isSuccess: false,
-      });
-    }
-  };;
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   setAlert({
+  //     message: "",
+  //     isSuccess: false,
+  //   });
+  //   const res = await createProperty(fields);
+  //   if (res.status === 201) {
+  //     setAlert({
+  //       message: "Success! Property added.",
+  //       isSuccess: true,
+  //     });
+  //   } else {
+  //     setAlert({
+  //       message: "Oops! Something went wrong. Property couldn't be added.",
+  //       isSuccess: false,
+  //     });
+  //   }
+  // };;
 
   const handleFieldChange = (event) => {
     setFields({ ...fields, [event.target.name]: event.target.value });
@@ -49,9 +47,6 @@ const AddProperty = () => {
 
   return (
     <div className="add-property">
-      {alert.message && (
-        <Alert message={alert.message} success={alert.isSuccess} />
-      )}
       <form onSubmit={handleSubmit}>
         <ul className="form-ul">
           <li>
--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,66 @@
 import '../styles/App.css';
-import React from 'react';
+import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import NavBar from './NavBar';
 import Properties from './Properties';
 import AddProperty from './AddProperty';
+import Alert from "./Alert";
+import createProperty from "../requests/createProperty";
 
 function App() {
+    const initialState = {
+      fields: {
+        title: "",
+        type: "Flat",
+        bedrooms: "",
+        bathrooms: "",
+        price: "",
+        city: "Manchester",
+        email: "",
+      },
+      alert: {
+        message: "",
+        isSuccess: false,
+      },
+    };
+    const [fields, setFields] = useState(initialState.fields);
+    const [alert, setAlert] = useState(initialState.alert);
+
+    const handleAddProperty = async (event) => {
+      event.preventDefault();
+      setAlert({
+        message: "",
+        isSuccess: false,
+      });
+      const res = await createProperty(fields);
+      if (res.status === 201) {
+        setAlert({
+          message: "Success! Property added.",
+          isSuccess: true,
+        });
+      } else {
+        setAlert({
+          message: "Oops! Something went wrong. Property couldn't be added.",
+          isSuccess: false,
+        });
+      }
+    };
   return (
     <div className="App">
       <NavBar />
-      <Switch> 
+      <Switch>
         <Route exact path="/">
           <Properties />
         </Route>
         <Route exact path="/add-property">
-          <AddProperty />
+          {alert.message && (
+            <Alert message={alert.message} success={alert.isSuccess} />
+          )}
+          <AddProperty
+            fields={fields}
+            setFields={setFields}
+            handleSubmit={handleAddProperty}
+          />
         </Route>
       </Switch>
     </div>
