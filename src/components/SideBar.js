6773a4e24398fec1f69b1633e204ae60b3b53166
Sam Tovey <samtovey13@gmail.com>
2021-02-10 01:10:52 +0000

    added buildQueryString function to parse and merge query strings into routes

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,33 @@
 import React, { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
+import qs from "qs";
 import "../styles/SideBar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSortUp,
+  faSortDown,
+  faFilter,
+  faPoundSign,
+} from "@fortawesome/free-solid-svg-icons";
 import getFilteredProperties from "../requests/getFilteredProperties";
 import Alert from './Alert';
 
+
+
 const Sidebar = ({ setProperties, errorMessage, setErrorMessage }) => {
   const { search } = useLocation();
+  const buildQueryString = (operation, valueObj) => {
+    const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+    const newQueryParams = {
+      ...currentQueryParams,
+      [operation]: JSON.stringify(valueObj),
+    };
+    return qs.stringify(newQueryParams, {
+      addQueryPrefix: true,
+      encode: false,
+    });
+  };
+
   useEffect(() => {
     async function loadProperties() {
       try {
@@ -32,18 +54,52 @@ const Sidebar = ({ setProperties, errorMessage, setErrorMessage }) => {
 
   return (
     <div className="side-bar">
-      <Link to={`/?query={"city": "Manchester"}`} className="side-bar-link">
-        Manchester
-      </Link>
-      <Link to={`/?query={"city": "Leeds"}`} className="side-bar-link">
-        Leeds
-      </Link>
-      <Link to={`/?query={"city": "Sheffield"}`} className="side-bar-link">
-        Sheffield
-      </Link>
-      <Link to={`/?query={"city": "Liverpool"}`} className="side-bar-link">
-        Liverpool
-      </Link>
+      <div className="side-bar-cities">
+        <div className="side-bar-link">
+          <FontAwesomeIcon icon={faFilter} />
+        </div>
+        <Link
+          to={buildQueryString("query", { city: "Manchester" })}
+          className="side-bar-link"
+        >
+          Manchester
+        </Link>
+        <Link
+          to={buildQueryString("query", { city: "Leeds" })}
+          className="side-bar-link"
+        >
+          Leeds
+        </Link>
+        <Link
+          to={buildQueryString("query", { city: "Sheffield" })}
+          className="side-bar-link"
+        >
+          Sheffield
+        </Link>
+        <Link
+          to={buildQueryString("query", { city: "Liverpool" })}
+          className="side-bar-link"
+        >
+          Liverpool
+        </Link>
+      </div>
+      <div className="side-bar-sorters">
+        <Link
+          to={buildQueryString("sort", { price: 1 })}
+          className="side-bar-link"
+        >
+          <FontAwesomeIcon icon={faSortUp} />
+        </Link>
+        <div className="side-bar-link">
+          <FontAwesomeIcon icon={faPoundSign} />
+        </div>
+        <Link
+          to={buildQueryString("sort", { price: -1 })}
+          className="side-bar-link"
+        >
+          <FontAwesomeIcon icon={faSortDown} />
+        </Link>
+      </div>
     </div>
   );
 };
