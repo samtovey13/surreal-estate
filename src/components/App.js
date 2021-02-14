445f1e8cbec8c6f3f8b88a7af8ab874d2d0b11ba
Sam Tovey <samtovey13@gmail.com>
2021-02-14 16:34:41 +0000

    added SSO to login with facebook. Buttons not yet styled.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import '../styles/App.css';
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import NavBar from './NavBar';
 import Properties from './Properties';
 import AddProperty from "./AddProperty";
 
 function App() {
+
+  const [userId, setUserId] = useState("");
+
+  const handleLogin = (response) => {
+    setUserId(response.id);
+  };
+
+  const handleLogout = () => {
+    window.FB.logout();
+    setUserId("");
+  };
+
   return (
     <div className="App">
-      <NavBar />
+      <NavBar onLogin={handleLogin} userId={userId} onLogout={handleLogout} />
       <div className="main-content">
         <Switch>
           <Route exact path="/">
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,40 @@ import React from "react";
 import '../styles/NavBar.css';
 import logo from '../images/logo.png';
 import { Link } from "react-router-dom";
+import FacebookLogin from "react-facebook-login";
 
-const NavBar = () => {
+const NavBar = ({ onLogin, userId, onLogout }) => {
   return (
     <div className="navbar">
-      <img className="logo" src={logo} alt="logo" width="80" height="100" />
-      <h1 className="header-name">Surreal Estate</h1>
+      <div className="header-flex-container">
+        <div className="header">
+          <img className="logo" src={logo} alt="logo" width="80" height="100" />
+          <h1 className="header-name">Surreal Estate</h1>
+        </div>
+        <div className="login-container">
+          {userId && (
+            (
+            (
+            <button className="logout-button" onClick={onLogout}>
+              
+              
+              Sign Out
+            
+            
+            </button>
+          )
+          )
+          )}
+          {!userId && (
+            <FacebookLogin
+              appId="298673424934631"
+              autoLoad={true}
+              fields="name,email,picture"
+              callback={onLogin}
+            />
+          )}
+        </div>
+      </div>
       <ul className="navbar-links">
         <li className="navbar-links-item">
           <Link to="/">View Properties</Link>
